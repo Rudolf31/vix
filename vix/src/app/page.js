1993d77f1606ba580b368dbf6391321d2db76b0b
Rudolf31 <rudolfshevchenko@gmail.com>
2024-08-28 17:09:30 +0300

    fixed images

--- vix/src/app/page.js
+++ vix/src/app/page.js
@@ -1,5 +1,7 @@
 import "./main_page.css";
 import * as images from "../app/images.js";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -7,21 +9,23 @@ export default function Home() {
       <header className="mt-5">
         <nav className="text-2xl font-bold justify-between px-10 pt-10">
           <ul className="flex gap-2 justify-between">
-            <li>Главная</li>
-            <li>Правила</li>
+            <li><Link href={""} >Главная</Link></li>
+            <li><Link href={""} >Правила</Link></li>
             <li>
               <div className="flex gap-2">
-                <img src={images.tg_icon} />
-                <img src={images.vk_icon} />
-                <img src={images.ds_icon} />
+                <Image src={images.tg_icon} />
+                <Image src={images.vk_icon} />
+                <Image src={images.ds_icon} />
               </div>
             </li>
-            <li>Профиль</li>
+            <li><Link href={""} className="profile align-middle" >Профиль</Link></li>
           </ul>
         </nav>
       </header>
       <main></main>
-      <footer></footer>
+      <footer>
+        <p>все права защищены</p>
+      </footer>
     </>
   );
 }
