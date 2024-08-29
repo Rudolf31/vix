32f46655c70db017c4bac482a6e8d27cbf484e3a
Rudolf31 <rudolfshevchenko@gmail.com>
2024-08-29 23:18:02 +0300

    work on the store

--- vix/src/app/components/buy_vix.js
+++ vix/src/app/components/buy_vix.js
@@ -1,14 +1,54 @@
 "use client";
 import "../styles/store.css";
+import { useState } from "react";
 
 export default function Store() {
+  const [amount, setAmount] = useState(10);
+  const [price, setPrice] = useState(10);
+
+  const max = 5000;
+
+  const handleChange = (e) => {
+    const value = Number(e.target.value);
+    if (value > max) {
+      setAmount(max);
+      setPrice(max);
+    } else if (value < 1) {
+      setAmount(1);
+      setPrice(1);
+    } else {
+      setAmount(value);
+      setPrice(value);
+    }
+  };
 
   return (
     <>
-        <h2 className="">Купить виксы</h2>
-        <div className="store min-h-36">
-
-        </div>
+      <h2 className="text-4xl text-left ml-5 mb-4">Купить виксы </h2>
+      <div className="store  flex ">
+        <input
+          className="bg-black px-3 py-2 text-4xl text-right"
+          type="number"
+          step="1"
+          min="1"
+          max={max}
+          value={amount}
+          id="amount"
+          name="amount"
+          onChange={handleChange}
+        />
+        <input
+          type="range"
+          step="1"
+          min="1"
+          max={max}
+          value={price}
+          id="price"
+          name="price"
+          onChange={handleChange}
+          className=""
+        />
+      </div>
     </>
   );
 }
